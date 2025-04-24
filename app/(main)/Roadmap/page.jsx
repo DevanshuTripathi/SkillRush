eef65303abcaecedc1ee577fb7b74c98c2f7c2d3
Shyam Jaiswal <jaiswal74882@gmail.com>
2025-04-24 15:46:14 +0530

    fixed

--- app/(main)/Roadmap/page.jsx
+++ app/(main)/Roadmap/page.jsx
@@ -1,10 +1,10 @@
 
 export default function RoadmapPage() {
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto py-10 ">
       <h1 className="text-4xl font-bold text-white mb-8 text-center">Roadmap</h1>
       <div className="bg-gray-800 rounded-xl p-4 shadow-lg overflow-auto">
-      <img src="frontend.svg" alt="Roadmap" className="w-full h-auto" />
+      <img src="/frontend.svg" alt="Roadmap" className="w-full max-w-4xl mx-auto h-auto"/>
 
       </div>
     </div>
